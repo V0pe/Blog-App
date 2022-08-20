b00f55b248af01884c62f88bf55ff3fcca8adb2b
V0pe <dampblew01@gmail.com>
2022-08-19 23:06:13 -0600

    add testing

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  #tests go here
+end
+
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  #tests go here
+end
+
